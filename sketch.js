9eb67807116f5d7604182539f6d5c174f93ef89d
Ashley Sheng <asheng03@gmail.com>
2021-07-29 17:29:15 -0500

    menu set up + medals

--- sketch.js
+++ sketch.js
@@ -4,12 +4,15 @@
 
 
 var s = function(sketch) {
-
+  let bronze;
+  let silver;
+  let gold;
   let col = "black";
-  let menuObj, gameMode, nightButton, dayImage, dayButton, nightImage;
-  let poopImage;
+  let menuObj;
+  let poopImage, poopList;
   let birdImage, flippedBirdImage, bird, birdOnScreen;
-
+  let points;
+  let thirdMedal, secondMedal, firstMedal;
   sketch.preload = function() {
     //load bird image facing left
     let filenameBird = "images/flying-bird.png"
@@ -25,6 +28,12 @@ var s = function(sketch) {
     let filenamePoop = "images/poop.png"
     let urlPoop = chrome.extension.getURL(filenamePoop)
     poopImage = sketch.loadImage(urlPoop)
+
+    //load medal images
+    firstMedal = sketch.loadImage(chrome.extension.getURL("images/gold-medal.png"))
+    secondMedal = sketch.loadImage(chrome.extension.getURL("images/silver-medal.png"))
+    thirdMedal = sketch.loadImage(chrome.extension.getURL("images/bronze-medal.png"))
+
 
   }
   sketch.setup = function() {
@@ -50,19 +59,39 @@ var s = function(sketch) {
     birdOnScreen = false;
 
     //gameMode = "day";
-    //menuObj = new Menu();
+    menuObj = new Menu();
     //nightButton = new Button(1, "night", birdImage)
+
+    poopList = [];
+    medalList = [];
+    points = 20
+    
+    bronze = new Medal(menuObj.x, menuObj.y + 125, thirdMedal, "Novice Pooper",  "You've collected 5 Poops!")
+    silver = new Medal(menuObj.x, menuObj.y + 225, secondMedal, "Intermediate Pooper", "You've collected 15 Poops!")
+    gold = new Medal(menuObj.x, menuObj.y + 325, firstMedal,  "Expert Pooper", "You've collected 20 Poops!")
   };
 
   sketch.draw = function() {
-    //menuObj.display()
+    
     //nightButton.display()
     
     sketch.lineArt()
     if(birdOnScreen){
       bird.moveBird()
       bird.display()
+      console.log(poopList)
+      if(sketch.frameCount % 120 == 0){
+        bird.addPoop()
+      }
+      for(let poop of poopList){
+        poop.display()
+        //error here!!
+        //poop.checkCollect()
+      }
     }
+    menuObj.display()
+    sketch.medalRewarder()
+
   };
   sketch.keyPressed = function(){
     if (sketch.key == 'e')
@@ -78,52 +107,97 @@ var s = function(sketch) {
     else if (sketch.key == 'p')
       bird = new Bird();
   };
+  class Poop {
+    constructor(xPos, yPos, image){
+      this.x = xPos
+      this.y = yPos
+      this.image = image
+    }
+    display(){
+      sketch.image(this.image, this.x, this.y, 30, 30)
+    }
+    checkCollect(){
+      //not working-- ask 
+      let collide = collideCircleCircle(this.x, this.y, 30, sketch.mouseX, sketch.mouseY, 3)
+      if(collide){
+        points+=1
+        let index = poopList.indexOf(this)
+        poopList.splice(index, 1)
+      }
+    }
+  };
   class Bird {
     constructor(){
-      this.xspeed = sketch.round(sketch.random(1, 3))
+      
       let randomNumber = sketch.round(sketch.random(0, 1))
-      console.log("random number: " + randomNumber)
+      
+      
+      this.xspeed = sketch.round(sketch.random(1, 3))
+      
       if(randomNumber == 1){
         this.x = sketch.windowWidth - 50
         this.xspeed *=-1
-        console.log("xspeed " + this.xspeed)
-        console.log("x: " + this.x)
         this.image = birdImage;
       } else {
         this.x = -50
-        console.log("xspeed: " + this.xspeed)
-        console.log("this.x: " + this.x)
         this.image = flippedBirdImage;
-        
       }
+
       this.y = sketch.random(100, sketch.height - 100)
       this.yspeed = sketch.random(2, 5)
+
       if(this.y > 250){
         this.yspeed *=-1
       }
-      console.log("yspeed: " + this.yspeed)
-      console.log("this.y: " + this.y)
+
       birdOnScreen = true;
+
+      /*console.log("random number: " + randomNumber)
+      console.log("xspeed " + this.xspeed)
+      console.log("x: " + this.x)
+      console.log("yspeed: " + this.yspeed)
+      console.log("this.y: " + this.y)*/
+    }
+    addPoop(){
+      let poop = new Poop(this.x, this.y, poopImage)
+      poopList.push(poop)
+      console.log("poop dropped")
     }
     moveBird(){
       this.x +=this.xspeed
       this.y +=this.yspeed 
-      console.log("yspeed: " + this.yspeed)
-      console.log("this.y: " + this.y)
-      console.log("xspeed: " + this.xspeed)
-      console.log("this.x: " + this.x)
+     
+      // 1 in 50 chance to switch directions
       let randomNumber = sketch.round(sketch.random(0, 50))
       if(randomNumber == 1){
         this.yspeed *= -1
       }
 
+      console.log("yspeed: " + this.yspeed)
+      console.log("this.y: " + this.y)
+      console.log("xspeed: " + this.xspeed)
+      console.log("this.x: " + this.x)
     }
     display(){
       sketch.clear();
       sketch.image(this.image, this.x, this.y, 100, 75)
-      
     }
   };
+  sketch.clearScreen = function(){
+    sketch.clear();
+
+  }
+  sketch.medalRewarder = function(){
+    if(points >=5){
+      bronze.display()
+    }
+    if (points >= 15){
+      silver.display()
+    }
+    if (points >=20){
+      gold.display()
+    }
+  }
   class Menu {
     constructor(){
       this.x = sketch.windowWidth - 250;
@@ -133,24 +207,35 @@ var s = function(sketch) {
       sketch.fill(60)
       sketch.stroke(60)
       sketch.rect(this.x, this.y, 200, 500)
+      sketch.fill("white")
+      sketch.textAlign(sketch.CENTER)
+      sketch.text("Collect the bird's poop for points!", this.x + 100, this.y + 10)
+      sketch.text("Points:", this.x + 100, this.y + 30)
+      sketch.image(poopImage, this.x + 100, this.y + 50, 30, 30)
+      sketch.text(points, this.x + 140, this.y + 75)
       //console.log("menu is drawn")
     }
   };
-  class Button {
-    constructor(number, name, image){
-      this.x = menuObj.x - number*20;
-      this.y = menuObj.y + number*20;
-      this.mode = name;
-      this.image = image
+  class Medal {
+    constructor(xPos, yPos, image, name, description){
+      this.x = xPos;
+      this.y = yPos;
+      this.image = image;
+      this.name = name;
+      this.description = description;
     }
     display(){
-      sketch.fill(100)
-      sketch.image(this.image, this.x, this.y, 10, 10)
-      sketch.fill(100)
-      sketch.text(this.mode, this.x + 10, this.y + 10)
+      sketch.fill("white")
+      sketch.textSize(15)
+      sketch.textAlign(sketch.CENTER)
+      sketch.text(this.name, this.x + 100, this.y + 65)
+      sketch.image(this.image, this.x + 100, this.y, 40, 50)
+      
+      sketch.textSize(10)
+      sketch.text(this.description, this.x + 100, this.y + 75)
     }
     checkCollide(){
-      let collide = collideCircleCircle(this.x, this.y, 10, 10, sketch.mouseX, sketch.mouseY, 3, 3)
+      let collide = sketch.collideCircleCircle(this.x, this.y, 10, 10, sketch.mouseX, sketch.mouseY, 3, 3)
       if(collide){
         gameMode = this.mode
       }
@@ -170,5 +255,6 @@ var s = function(sketch) {
     }
   }
 };
+
 
 var myp5 = new p5(s);
